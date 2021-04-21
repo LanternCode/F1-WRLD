39578ad59f252ead6a1e9a4f3b912272baecd6dd
LanternCode <Adam.Machowczyk@gmail.com>
2021-04-21 13:38:19 +0100

    Added a footer to the homepage.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,5 @@ menuToggler.addEventListener('click', ev => {
   menu.classList.toggle('open');
   menuToggler.classList.toggle('open');
   main.classList.toggle('open');
+  footer.classList.toggle('open');
 });
